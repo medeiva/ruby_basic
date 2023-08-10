d5964c325a0266145396d9a9ae2cf3e9bb9355d7
medeiva <medeiva91@yandex.ru>
2023-08-10 21:46:08 +0200

    lesson 2

---
+++ lesson2/shop.rb
@@ -0,0 +1,23 @@
+hash = {}
+loop do 
+  puts "Input product"
+  product = gets.chomp.downcase
+  break if product == "stop"
+  
+  puts "Input unit price"
+  price = gets.chomp.to_i
+  
+  puts "Input count"
+  count = gets.chomp.to_i
+
+  hash[product] = { price: price, count: count }
+end
+
+sum = 0
+hash.each do |key, value|	
+  product_sum = value[:price] * value[:count]
+  sum += product_sum
+  puts "product: #{key}, price: #{value[:price] }, count: #{value[:count]}, sum: #{product_sum}"
+end
+
+puts "Purchase amount: #{sum}"
