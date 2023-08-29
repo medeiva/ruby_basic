51cb9dc832b991b8e5fa5e6c50415f049cc8800f
medeiva <medeiva91@yandex.ru>
2023-08-29 21:52:13 +0200

    fix lesson3

--- lesson3/train.rb
+++ lesson3/train.rb
@@ -1,6 +1,6 @@
 class Train
   attr_accessor :speed
-  attr_reader :count_carriage, :type, :station
+  attr_reader :count_carriage, :type, :current_station_index, :current_station, :route
 
   def initialize(number, type, count_carriage)
     @number = number
@@ -21,35 +21,37 @@ class Train
     @count_carriage -= 1 if @speed == 0
   end
 
-  def put_route(route)
+  def assigne_route(route)
     @route = route
-    @station =  @route.start_station
-    @station.take_train(self)
+    @current_station_index = 0
+    @current_station = @route.stations.first
+    @current_station.take_train(self)
   end
 
   def next_station
-    @route.list_station[index_station + 1]
+    @route.stations[current_station_index + 1]
   end
 
   def prev_station
-    @route.list_station[index_station - 1]
+    @route.stations[current_station_index - 1]
   end
 
   def go_to_next_station
-    move_to_station(next_station) if @station != @route.end_station
+    move_to_station(next_station, current_station_index + 1) if current_station != @route.stations.last
   end
 
   def go_to_prev_station
-    move_to_station(prev_station) if @station != @route.start_station
+    move_to_station(prev_station, current_station_index - 1) if current_station != @route.stations.first
   end
 
-  def move_to_station(new_station)
-      @station.send_train(self)
-      @station = new_station
-      @station.take_train(self)
+  def move_to_station(new_station, index)
+    @current_station_index = index
+    @current_station.send_train(self)
+    @current_station = new_station
+    @current_station.take_train(self)
   end
 
   def index_station
-    @route.list_station.index(@station)
+    @route.stations.index(self.station)
   end
 end
